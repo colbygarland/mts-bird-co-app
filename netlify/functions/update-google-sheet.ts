cde7caf044d8f17d0024c23292eaa2e0f38e3abc
Colby Garland <colbygarland@gmail.com>
2022-01-14 14:21:11 -0800

    changes

--- netlify/functions/update-google-sheet.ts
+++ netlify/functions/update-google-sheet.ts
@@ -3,7 +3,7 @@ import { Handler } from '@netlify/functions';
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello world' }),
+    body: JSON.stringify({ message: 'Hello world!' }),
   };
 };
 
